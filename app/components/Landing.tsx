e37983df99edeb620217020b050ebd70dcfab840
Kulani Baloyi <kulani17.kb@gmail.com>
2025-12-09 12:02:07 +0200

    remove boiler plate code

---
+++ app/components/Landing.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+   <section className='h-screen grid place-content-center'>
+    <h1 className='text-5xl font-bold '>Tsakani and Harold</h1>
+   </section>
+  )
+}
+
+export default Landing
